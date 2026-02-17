bea9ce0a1289948bd2b897da71d7e872ad8c5610
drichman1-maker <douglasrichman1@gmail.com>
2026-02-17 18:17:46 -0500

    Add privacy policy page for affiliate compliance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Layout/Navbar'
+import Footer from './components/Layout/Footer'
 import Home from './pages/Home'
 import ProductCatalog from './pages/ProductCatalog'
 import ProductDetail from './pages/ProductDetail'
+import Privacy from './pages/Privacy'
 
 function App() {
   return (
@@ -15,8 +17,10 @@ function App() {
           <Route path="/products" element={<ProductCatalog />} />
           <Route path="/products/:category" element={<ProductCatalog />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   )
 }
